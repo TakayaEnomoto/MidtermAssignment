b045aa515027f3219ec78d7771faa8527d4a3dee
TakayaEnomoto <49789142+TakayaEnomoto@users.noreply.github.com>
2021-03-15 07:58:24 +0800

    Added a level generator along with a class to assist generating.

---
+++ Assets/Scripts/ColorToPrefab.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+//This class is credit to https://www.youtube.com/watch?v=B_Xp9pt8nRY
+[System.Serializable]
+public class ColorToPrefab 
+{
+    public Color color;
+    public GameObject prefab;
+}
