b08a9d494eb7f73f14b9d378d66a0630cd08d309
TakayaEnomoto <49789142+TakayaEnomoto@users.noreply.github.com>
2021-03-15 10:07:07 +0800

    Added scripts to controll buttons and doors.

---
+++ Assets/Scripts/SquareButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareButton : MonoBehaviour
+{
+    SpriteRenderer sr;
+    public bool active;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            sr.color = Color.green;
+            active = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            sr.color = new Color(1, 0.1462264f, 0.1462264f);
+            active = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/SquareDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareDoor : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject button;
+    private float t;
+    void Start()
+    {
+        button = GameObject.Find("Button(Square)");
+    }
+
+    
+    void Update()
+    {
+        transform.localScale = new Vector3(Mathf.Lerp(1, 0, t), transform.localScale.y);
+        OpenDoor();
+        if (t > 1)
+            t = 1;
+        if (t < 0)
+            t = 0;
+    }
+
+    void OpenDoor()
+    {
+        if (button.GetComponent<SquareButton>().active)
+        {
+            t += Time.deltaTime * 1.5f;
+        }
+        else if (button.GetComponent<SquareButton>().active == false)
+        {
+            t -= Time.deltaTime * 1.5f;
+        }
+    }
+}
